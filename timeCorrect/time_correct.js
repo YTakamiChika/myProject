3aa984831c0e2e895c7dcb8ffc3b5073650e2834
轻飘飘 <379272097@qq.com>
2019-11-18 11:22:33 +0800

    更新了版本v1.0.1

--- timeCorrect/time_correct.js
+++ timeCorrect/time_correct.js
@@ -1,6 +1,11 @@
 //时间校正函数，主要用于js计时器出现的时间误差情况（倒计时或者正计时）
+/*
+  版本更新 v1.0.1 日期：2019.11.20
+  更新日志 修复了开启关闭时候的bug
+          给传入的时间增加了一个默认值
+ */
 function TimeCorrect() {
-    this.start = true; //开关
+    this.start = false; //开关
 }
 
 /**
@@ -10,21 +15,25 @@ function TimeCorrect() {
  **/
 TimeCorrect.prototype.setInterval = function (fn,delay) {
     //延迟必须为数字类型
-    if(isNaN(delay))  return;
+    if(isNaN(delay)){
+      delay = 1000;
+    }
     var context = this;
-
+    context.start = true;
+    context.fn = fn;
+    context.delay = delay;
     let real_time = delay; //矫正后的执行时间
     function timerNext(){
-        fn.call(context);
         const oldtime = new Date().getTime();
-        let timer = setTimeout(() => {
+        context.timer = setTimeout(() => {
+            fn.call(context);
             const newtime = new Date().getTime();
             const dtime = newtime - oldtime;
             real_time = delay + (real_time - dtime);
             if(context.start){
                 timerNext();
             }else{
-                clearTimeout(timer);
+                clearTimeout(context.timer);
             }
         },real_time)
     }
@@ -32,16 +41,14 @@ TimeCorrect.prototype.setInterval = function (fn,delay) {
 }
 
 //启动
-TimeCorrect.prototype.begin = function (fn,delay) {
+TimeCorrect.prototype.begin = function () {
     if(this.start) return;
-    this.start = true;
-    this.setInterval(fn,delay);
+    this.setInterval(this.fn,this.delay);
 }
 
 
 //停止
 TimeCorrect.prototype.stop = function () {
     this.start = false;
+    clearTimeout(this.timer);
 }
-
-
